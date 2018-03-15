acf91ff21c79ae91abafe8efec75e96616d487fb
Francisco Giovanildo Teixeira de Souza <giovanildos@unilab.edu.br>
2018-03-15 16:22:31 -0300

    debugando codigo Liga.java

--- src/main/Partidas.java
+++ src/main/Partidas.java
@@ -2,39 +2,42 @@ package main;
 
 import java.util.ArrayList;
 
+// pergunta o nome do torneio
+// pergunta o nome do e-atleta e o time que ele vai jogar or sorteia o
+// time
+// adiciona em um array
+// monta as rodadas e a tabela de pontos
+
 public class Partidas {
 
 	public static void main(String[] args) {
-		geraTabela();
+		// geraTabela();
+		
 	}
 
+	
 	public static void geraTabela() {
-		ArrayList<EAtletaTorneio> lista = preencherListaAtletaTorneio();
-		int contar = 0;
-		for (int a = 0; a < lista.size(); a++) {
-			for (int b = 0; b < lista.size(); b++) {
-				if (a != b) {
-					System.out.println(" " + contar + " anfitrião " + a + " x " + b + " visitante ");
-					contar++;
-				}
-				
+		// fazer rodadas do torneio
+		// guarda em uma matriz as combinações, uma rodada tem que todos os times
+		// jogarem
+		// uma rodada sempre será a metade do número de participantes
+		
 
-			}
-		}
+		// 1 x 7 - 2 x
 
-		/*
-		 * 0 x 1 0 x 2 0 x 3 1 x 0 1 x 2 1 x 3 2 x 0 2 x 1 2 x 3 3 x 0 3 x 1 3 x
-		 * 2
-		 */
+		// char contar = 'a';
 
-		// pergunta o nome do torneio
-
-		// pergunta o nome do e-atleta e o time que ele vai jogar or sorteia o
-		// time
-
-		// adiciona em um array
-
-		// monta as rodadas e a tabela de pontos
+		// for (int a = 0; a < lista.size(); a++) {
+		// for (int b = 0; b < lista.size(); b++) {
+		// if (a != b) {
+		// System.out.println(" " + contar + " anfitrião " + a + " x " + b + " visitante
+		// ");
+		// contar++;
+		// }
+		//
+		//
+		// }
+		// }
 	}
 
 	public static ArrayList<EAtletaTorneio> preencherListaAtletaTorneio() {
@@ -43,6 +46,16 @@ public class Partidas {
 		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("gen"), new Clube("chelsea")));
 		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("zal"), new Clube("liverpool")));
 		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("fab"), new Clube("real")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("assis"), new Clube("roma")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("nen"), new Clube("barça")));
+
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("nana"), new Clube("milan")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("natan"), new Clube("inter")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("mario 20"), new Clube("toteran")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("barbosa"), new Clube("manchester united")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("crocodilo"), new Clube("manchester city")));
+		lista.add(new EAtletaTorneio(new Torneio("La decima"), new EAtleta("pedro henrique"),
+				new Clube("atletico de madrid")));
 		return lista;
 
 	}
