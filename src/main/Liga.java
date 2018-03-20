f49d83c05822d39a68d1e8bfc6dbe6ec1af6bdcf
Francisco Giovanildo Teixeira de Souza <giovanildos@gmail.com>
2018-03-20 01:04:50 -0300

    testando usando variavel string, e mostrando sysout no final

--- src/main/Liga.java
+++ src/main/Liga.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Liga {
 
 	public static void main(String[] args) {
-		System.out.println(2%1.5);
 		liga();
 	}
 
@@ -14,8 +13,8 @@ public class Liga {
 		// cria um array de clubes e preenchendo
 		ArrayList<String> clubes = new ArrayList<String>();
 
-		int qtdPovoar = 11; // número de clubes - é usado somente para povoar a array de clubes
-		for (int i = 0; i < qtdPovoar ; i++) {
+		int qtdPovoar = 8; // número de clubes - é usado somente para povoar a array de clubes
+		for (int i = 0; i < qtdPovoar; i++) {
 			clubes.add("Club_" + Integer.toString(i));
 		}
 
@@ -33,67 +32,51 @@ public class Liga {
 		int totalP = (qtdClubes * (qtdClubes - 1)) / 2; // total de partidas por turno
 		String[] casa = new String[totalP]; // criando matrizes com o número de partidas por turno
 		String[] fora = new String[totalP];// criando matrizes com o número de partidas por turno
-
+		
 		// fazendo a tabela
-		int modIF = (qtdClubes / 2);// para hacer mod cada inicio de data - criado para tirar o resto da divisão
+		int modIF = (qtdClubes / 2);// usado para fazer as combinações
 		int indiceInverso = qtdClubes - 2;
 		for (int i = 0; i < totalP; i++) {
-			System.out.println(i);
-			System.out.println(" i " + i + " modif " + modIF +  " resultado " + i % modIF );
-			if (i % modIF == 0) {// seria el partido inicial de cada data				
-				System.out.println(fora[i] + " vs " + casa[i] + "\n");
+			String onde = "";	
+			if (i % modIF == 0) {// seria el partido inicial de cada data
 				if (impar) { // qtd de clubes impar
-
 					casa[i] = null;
 					fora[i] = null;
-					System.out.println("qtd Clubes " + qtdClubes);// aqui aparece o valor impar acrescido de mais um.
-																	// Por isso é anulado
-					System.out.println(
-							" anulando o valor acrescido no caso de ser impar " + fora[i] + " vs " + casa[i] + "\n");
-				} else {// qtd de clubes par
-					// coloca um em casa e outro fora
-					System.out.println("quando a qtd de clubes é par");
+				} else {// qtd de clubes par coloca um em casa e outro fora
 					if (i % 2 == 0) {
 						casa[i] = clubes.get(i % (qtdClubes - 1));
 						fora[i] = clubes.get(qtdClubes - 1);
-						System.out.println(" i resto com 2");
-						System.out.println(fora[i] + " vs " + casa[i] + "\n");
+						onde = "é par";						
 					} else {
 						casa[i] = clubes.get(qtdClubes - 1);
 						fora[i] = clubes.get(i % (qtdClubes - 1));
-						System.out.println(" sou par mas o resto não é 0");
-						System.out.println(fora[i] + " vs " + casa[i] + "\n");
 					}
 				}
 			} else {
 				casa[i] = clubes.get(i % (qtdClubes - 1));
 				fora[i] = clubes.get(indiceInverso);
-				System.out.println(" i resto com a metade da qtd de clubes diferente de 0");
-				System.out.println(fora[i] + " vs " + casa[i] + "\n");
 				--indiceInverso;
 				if (indiceInverso < 0) {
 					indiceInverso = qtdClubes - 2;
-					System.out.println(
-							" se o indice inverso for menor que zero atribuindo novo valor que é a qtd de clubes decrescido de 2");
-					System.out.println(fora[i] + " vs " + casa[i] + "\n");
 				}
 			}
+			System.out.println(i + " " + onde);
 		}
 
+		
 		// mostra a tabela percorrendo as duas matrizes de
-		System.out.println("\n Primeiro Turno \n \n");
-		for (int i = 0; i < totalP; i++) {
-			if (casa[i] != null) {// corrige o caso de números impares
-				System.out.println(casa[i] + " vs " + fora[i] + "\n");
-			}
-		}
-
-		System.out.println("\n Segundo Turno \n \n");
-		for (int i = 0; i < totalP; i++) {// corrige o caso de números impares
-			if (casa[i] != null) {
-				System.out.println(fora[i] + " vs " + casa[i] + "\n");
-			}
-		}
+//		for (int i = 0; i < totalP; i++) {
+//			if (casa[i] != null) {// corrige o caso de números impares
+//				System.out.println(casa[i] + " vs " + fora[i] + "\n");
+//			}
+//		}
+//
+//		System.out.println("\n Segundo Turno \n \n");
+//		for (int i = 0; i < totalP; i++) {// corrige o caso de números impares
+//			if (casa[i] != null) {
+//				System.out.println(fora[i] + " vs " + casa[i] + "\n");
+//			}
+//		}
 	}
 
 }
