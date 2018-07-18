d7673d1ab9d732441ab55d95472703547ffc6c81
giovanildo <giovanildos@gmail.com>
2018-07-18 13:17:55 -0300

    Ajustando o script para aceitar turno e returno

--- src/main/Partidas.java
+++ src/main/Partidas.java
@@ -11,19 +11,8 @@ public class Partidas {
 	private List<String> clubes = new ArrayList<String>();
 
 	/**
-	 * Métodos para adicionar clubes a partir de uma entrada scanner
+	 * MÃ©todos para adicionar clubes a partir de uma entrada scanner
 	 */
-	public void teste() {
-		for (int contador = 1; contador <= 100; contador++) {
-			if (contador % 5 != 0)
-				continue;
-			/*
-			 * Se o contador não for múltiplo de 5 Todo o código abaixo será ignorado e o
-			 * loop continua com o próximo nr
-			 */
-			System.out.println("Contador: " + contador);
-		}
-	}
 
 	public void adicionarClubes() {
 		System.out.println("Entre com o nome dos clubes. Deixe em branco para terminar.");
@@ -48,7 +37,7 @@ public class Partidas {
 	}
 
 	public void gerarTabela() {
-		// variaveis que serão base para gerar tabela
+		// variaveis que serao base para gerar tabela
 		int totalClubes = clubes.size();
 		int metadeClubes = totalClubes / 2;
 		int mando = 0;
@@ -57,39 +46,54 @@ public class Partidas {
 		int m0 = 0;
 		String remove = "";
 		// montando a tabela
-		for (int t = 0; t < (totalClubes - 1); t++) {// for das rodadas
-			System.out.print((t + 1) + "a rodada: ");
-			for (int m = 0; m < metadeClubes; m++) {// for dos jogos
-				// Clube está de fora nessa rodada?
-				if (clubes.get(m).isEmpty())
-					continue;
-				mando = totalClubes - m - 1;
-				m1 = m % 2;
-				t1 = t % 2;
-				m0 = m;
-				// Teste para ajustar o mando de campo
-				if (m % 2 == 1 || t % 2 == 1 && m == 0) {
-					System.out.print(clubes.get(mando) + " x " + clubes.get(m) + "   ");
-				} else {
-					System.out.print(clubes.get(m) + " x " + clubes.get(mando) + "   ");
+
+		for (int r = 0; r < 2; r++) {
+			for (int t = 0; t < (totalClubes - 1); t++) {// for das rodadas
+				System.out.print((t + 1) + "a rodada: ");
+				for (int m = 0; m < metadeClubes; m++) {// for dos jogos
+					// Clube estÃ¡ de fora nessa rodada?
+					if (clubes.get(m).isEmpty())
+						continue;
+					mando = totalClubes - m - 1;
+					m1 = m % 2;
+					t1 = t % 2;
+					m0 = m;
+					// Teste para ajustar o mando de campo
+					if (m % 2 == 1 || t % 2 == 1 && m == 0) {
+						if(r==0) {
+							System.out.print(clubes.get(mando) + " x " + clubes.get(m) + "   ");	
+						} else {
+							System.out.print(clubes.get(m) + " x " + clubes.get(mando) + "   ");
+						}
+						
+					} else {
+						if(r==1) {
+							System.out.print(clubes.get(m) + " x " + clubes.get(mando) + "   ");
+						} else {
+							System.out.print(clubes.get(mando) + " x " + clubes.get(m) + "   ");
+						}
+							
+						
+					}
 				}
+				System.out.println();
+				remove = clubes.remove(clubes.size() - 1);
+				// Gira os clubes no sentido horÃ¡rio, mantendo o primeiro no lugar
+				clubes.add(1, remove);// apaga o clube do final do array e o insere na segunda posiÃ§Ã£o
 			}
-			System.out.println();
-			remove = clubes.remove(clubes.size() - 1);
-			// Gira os clubes no sentido horário, mantendo o primeiro no lugar
-			clubes.add(1, remove);//apaga o clube do final do array e o insere na segunda posição
 		}
+
 	}
 
 	public void partidas() {
 		// adicionarClubes();
-		 clubes.add("0");
-		 clubes.add("1");
-		 clubes.add("2");
-		 clubes.add("3");		 
-		 
-		 qtdClubesImpar();
-		 gerarTabela();
-		
+		clubes.add("Genilson");
+		clubes.add("Fabiano");
+		clubes.add("Zaldir");
+		clubes.add("Giovanildo");
+
+		qtdClubesImpar();
+		gerarTabela();
+
 	}
 }
