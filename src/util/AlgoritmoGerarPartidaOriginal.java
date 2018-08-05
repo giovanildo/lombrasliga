81c3ac8e8778864a03d0b0d2663b1d7b12f70186
Francisco Giovanildo Teixeira de Souza <giovanildos@gmail.com>
2018-08-04 21:26:56 -0300

    renomeando para não confundir classe do Algoritmo Gerar Partida Original foi aprimorado e orientado a objeto.

--- src/util/AlgoritmoGerarPartidaOriginal.java
+++ src/util/AlgoritmoGerarPartidaOriginal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Partidas {
+public class AlgoritmoGerarPartidaOriginal {
 
 	private Scanner in;
 	List<String> clubes;
