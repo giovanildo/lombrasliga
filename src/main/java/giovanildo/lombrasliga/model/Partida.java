1ae1dc95f249457909fb02dcb672ec640a3188b8
Francisco Giovanildo Teixeira de Souza <giovanildos@unilab.edu.br>
2018-11-23 16:13:25 -0300

    Removendo referencias da classe equipe em campo e colocando na classe Partida

--- src/main/java/giovanildo/lombrasliga/model/Partida.java
+++ src/main/java/giovanildo/lombrasliga/model/Partida.java
@@ -1,6 +1,6 @@
 package giovanildo.lombrasliga.model;
 
-import giovanildo.lombrasliga.model.EquipeEmCampo;;
+
 /**
  * 
  * @author giovanildo 
@@ -15,27 +15,147 @@ public class Partida {
 	/**
 	 * clube que joga em casa
 	 */
-	private EquipeEmCampo anfitriao;
+	private EAtletaTorneio anfitriao;
 	/**
 	 * clube que joga fora
 	 */
-	private EquipeEmCampo visitante;
+	private EAtletaTorneio visitante;
 	/**
 	 * saber se a partida foi encerrada ou não
 	 */
+	
 	private boolean encerrada;
-
+	/**
+	 * Gols do Visitante
+	 */
+	private int golsVisitante;
+	/**
+	 * Gols do Anfitriao
+	 */
+	private int golsAnfitriao;
+	
 	/**
 	 * 
 	 * @param anfitriao
 	 * @param visitante
 	 */
-	public Partida(EquipeEmCampo anfitriao, EquipeEmCampo visitante) {
+	public Partida(EAtletaTorneio anfitriao, int golsAnfitriao, EAtletaTorneio visitante, int golsVisitante) {
 		this.anfitriao = anfitriao;
+		this.visitante = visitante;
+		this.golsAnfitriao = golsAnfitriao;
+		this.golsVisitante = golsVisitante;
+	}
+	
+
+	/**
+	 * 
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return o anfitrião da partida
+	 */
+
+	public EAtletaTorneio getAnfitriao() {
+		return anfitriao;
+
+	}
+
+	/**
+	 * 
+	 * @return a equipe que joga fora de casa
+	 */
+	public EAtletaTorneio getVisitante() {
+		return visitante;
+	}
+	/**
+	 * 
+	 * @param anfitriao
+	 */
+	public void setAnfitriao(EAtletaTorneio anfitriao) {
+		this.anfitriao = anfitriao;
+	}
+	/**
+	 * 
+	 * @param visitante
+	 */
+
+	public void setVisitante(EAtletaTorneio visitante) {
 		this.visitante = visitante;
 	}
 
+	/**
+	 * 
+	 * @param golsVisitante
+	 */
+	public void setGolsVisitante(int golsVisitante) {
+		this.golsVisitante = golsVisitante;
+	}
+	/**
+	 * 
+	 * @param golsAnfitriao
+	 */
 
+	public void setGolsAnfitriao(int golsAnfitriao) {
+		this.golsAnfitriao = golsAnfitriao;
+	}
+	
+
+	/**
+	 * @return the encerrada verifica se a partida está encerrada
+	 */
+	public boolean isEncerrada() {
+
+		return encerrada;
+	}
+
+	/**
+	 * @param encerrada the encerrada to set
+	 */
+	public void setEncerrada(boolean encerrada) {
+		this.encerrada = encerrada;
+	}
+	
+	/**
+	 * 
+	 * @return qtd gols do visitante
+	 */
+	public int getGolsVisitante() {
+		return golsVisitante;
+	}
+
+	/**
+	 * 
+	 * @return qtd gols do anfitriao
+	 */
+	public int getGolsAnfitriao() {
+		return golsAnfitriao;
+	}
+
+	@Override
+	public String toString() {
+		String clubeMandante = anfitriao.getClube().getNome();
+		String clubeVisitante = visitante.getClube().getNome();
+		String eAtletaMandante = anfitriao.geteAtleta().getNome().toUpperCase();
+		String eAtletaVisitante = visitante.geteAtleta().getNome().toUpperCase();
+		
+		int golsMandante = this.golsAnfitriao;
+		int golsVisitante = this.golsVisitante;		
+		
+		return  eAtletaMandante + " - " + clubeMandante + " " + golsMandante + " x " + golsVisitante + " " + clubeVisitante + " - " + eAtletaVisitante;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -59,69 +179,5 @@ public class Partida {
 			return false;
 		return true;
 	}
-
-
-	/**
-	 * 
-	 * @return id
-	 */
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * 
-	 * @param id
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return o anfitrião da partida
-	 */
-
-	public EquipeEmCampo getAnfitriao() {
-		return anfitriao;
-
-	}
-
-
-	/**
-	 * 
-	 * @return a equipe que joga fora de casa
-	 */
-	public EquipeEmCampo getVisitante() {
-		return visitante;
-	}
-
-	/**
-	 * @return the encerrada verifica se a partida está encerrada
-	 */
-	public boolean isEncerrada() {
-
-		return encerrada;
-	}
-
-	/**
-	 * @param encerrada the encerrada to set
-	 */
-	public void setEncerrada(boolean encerrada) {
-		this.encerrada = encerrada;
-	}
-	@Override
-	public String toString() {
-		String clubeMandante = anfitriao.geteAtletaTorneio().getClube().getNome();
-		String clubeVisitante = visitante.geteAtletaTorneio().getClube().getNome();
-		String eAtletaMandante = anfitriao.geteAtletaTorneio().geteAtleta().getNome().toUpperCase();
-		String eAtletaVisitante = visitante.geteAtletaTorneio().geteAtleta().getNome().toUpperCase();
-		
-		int golsMandante = anfitriao.getGols();
-		int golsVisitante = visitante.getGols();		
-		
-		return  eAtletaMandante + " - " + clubeMandante + " " + golsMandante + " x " + golsVisitante + " " + clubeVisitante + " - " + eAtletaVisitante;
-	}
-
-
 
 }
