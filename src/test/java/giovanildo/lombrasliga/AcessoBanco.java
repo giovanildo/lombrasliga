6171ac7f544cbe66a5d642c8f44bfed92733059f
Francisco Giovanildo Teixeira de Souza <giovanildos@unilab.edu.br>
2018-11-23 16:10:04 -0300

    classes a serem estudadas

--- src/test/java/giovanildo/lombrasliga/AcessoBanco.java
+++ src/test/java/giovanildo/lombrasliga/AcessoBanco.java
@@ -8,21 +8,21 @@ import java.sql.SQLException;
 
 public class AcessoBanco {
 	public static void main(String[] args) {
-		String url = "jdbc:postgresql://127.0.0.1/xti";
-		String sql = "select * from pessoa";
-		String usuario = "postgres";
-		String senha = "labti@unilab2012";
-		try (Connection con = DriverManager.getConnection(url, usuario, senha);
-				PreparedStatement stm = con.prepareStatement(sql);
-				ResultSet rs = stm.executeQuery()) {
-			while (rs.next()) {
-				System.out.println(rs.getString("nome") + " " + rs.getString("email"));
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		String url = "jdbc:postgresql://127.0.0.1/xti";
+//		String sql = "select * from pessoa";
+//		String usuario = "postgres";
+//		String senha = "labti@unilab2012";
+//		try (Connection con = DriverManager.getConnection(url, usuario, senha);
+//				PreparedStatement stm = con.prepareStatement(sql);
+//				ResultSet rs = stm.executeQuery()) {
+//			while (rs.next()) {
+//				System.out.println(rs.getString("nome") + " " + rs.getString("email"));
+//			}
+//
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 	}
 
