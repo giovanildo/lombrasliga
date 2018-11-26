da69de294f8a96746fe6c8bd14c1b5a36b5eb116
giovanildo <giovanildo@gmail.com>
2018-11-26 16:31:19 -0300

    Criando script sql para criar a base de dados.

--- src/main/java/giovanildo/lombrasliga/dao/DAO.java
+++ src/main/java/giovanildo/lombrasliga/dao/DAO.java
@@ -1,31 +1,30 @@
 package giovanildo.lombrasliga.dao;
 
 public class DAO {
-	
-	
+
 	public void criarBanco() {
 //		criar banco de dados lombras
-		String sql = "CREATE DATABASE lombras OWNER postgres ENCODING 'UTF-8';"
-				+ "CREATE TABLE [IF NOT EXISTS] EAtleta (\r\n" + 
-				"  id int [COLLATE colação] constraint,\r\n" + 
-				"  nome_coluna tipo_dados constraint,\r\n" + 
-				"  nome_coluna tipo_dados constraint,\r\n" + 
-				"  ...,\r\n" + 
-				"[FOREIGN KEY chave_estrangeira REFERENCES coluna]\r\n" + 
-				"[ON DELETE ação ] [ ON UPDATE ação ]\r\n" + 
-				");"
-				+ ""
-				+ "";
+		String sql = "CREATE DATABASE lombras OWNER postgres ENCODING 'UTF-8';";
 //		tabela eatleta
-		
-//		id e nome
+//		id e nome		
+		String eatleta = "CREATE TABLE eatleta (id_eatleta integer CONSTRAINT pk_id_eatleta PRIMARY KEY, "
+				+ " nome_eatleta varchar(30) NOT NULL, );";
 //		tabela clube
 //		id e nome
+		String clube = "CREATE TABLE clube (id_clube integer CONSTRAINT pk_id_clube PRIMARY KEY, "
+				+ " nome_clube varchar(30) NOT NULL, );";
 //		tabela torneio
 //		id e nome e porquedoNome
+		String torneio = "CREATE TABLE torneio (id_torneio integer CONSTRAINT pk_id_torneio PRIMARY KEY, "
+				+ " nome_torneio varchar(30) NOT NULL, );";
+		
 //		tabela EatletaTorneio
 //		id, id eatleta, id clube, id torneio
-//
+		
+		String eatletatorneio = "CREATE TABLE eatletaTorneio (id_clube integer CONSTRAINT pk_id_clube PRIMARY KEY, "
+				+ " nome_clube varchar(30) NOT NULL, );";
+		
+		
 //		tabela partida
 //		id
 //		visitante
@@ -33,8 +32,7 @@ public class DAO {
 //		golsVisitante
 //		golsAnfitriao
 //		encerrada?
-		
-		
+
 	}
 
 }
